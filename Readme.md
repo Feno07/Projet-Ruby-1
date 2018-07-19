b8a41d29b9c2563af704ea227ee35f8684034414
Feno07 <m.fenohery07@gmail.com>
2018-07-19 10:59:26 +0200

    Création de variable

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Pour ce premier test nous allons montrer les possibilités que montre la language ruby
+A la ligne 3 montre la condition if ou si en français
+La ligne 14 permet d'obtenir une interaction avec le client
---
+++ ruby.rb
@@ -0,0 +1,34 @@
+# une devinette
+puts "Trouver un chiffre entre 1 à 20"
+i = gets.chomp.to_i 
+   if i < 13
+        puts "Faux"
+    elsif i > 13
+        puts "Encore faux"
+    else
+        puts "Vrai"
+    end 
+# Donner votre nom
+
+puts "Entrer votre nom:"
+nom = gets.chomp.downcase
+
+# un peu de mathématique
+puts "2 + 6"
+sum = gets.chomp.to_i
+    if sum == 8
+        puts "Bien"
+    else sum != 8
+        puts "Revoir votre réponse"
+    end
+# ceci clotûre ma toute première projet
+
+
+
+
+
+
+
+
+
+
